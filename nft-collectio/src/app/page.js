c8e530eee2a9bc642951e137916784676d616306
smoke-hux <huxleyomondu@live.com>
2023-07-17 03:16:08 +0300

    working one

--- nft-collectio/src/app/page.js
+++ nft-collectio/src/app/page.js
@@ -1,115 +1,111 @@
 'use client'
-import Image from 'next/image'
-import styles from './page.module.css'
-import {Contract, providers, utils} from "ethers";
-import React, {useEffect, useRef, useState } from "react"
+import { Contract, providers, utils } from "ethers";
 import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import { abi, NFT_CONTRACT_ADDRESS } from "/constants";
+import styles from "/styles/Home.module.css";
 
 export default function Home() {
-
-  // connect wallet to keep track on whether the user's wallet is connected.
-
+  // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
-
-  // leading is set to true when we are waiting for a transaction to get mined
-
+  // loading is set to true when we are waiting for a transaction to get mined
   const [loading, setLoading] = useState(false);
-
-  // tokenIdMinted keeps track of the number of tokeIds that have been minted
-
-  const [tokenIdsMinted, setTokenIdsMinted] = useState('0');
-
-  //create a reerence to the web3modal (used for connecting to Metamask as long as the page is open
-
+  // tokenIdsMinted keeps track of the number of tokenIds that have been minted
+  const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
+  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
   const web3ModalRef = useRef();
 
-  // mint an nft : public mint
-
-  const publicMint = async ()  => {
+  /**
+   * publicMint: Mint an NFT
+   */
+  const publicMint = async () => {
     try {
       console.log("Public mint");
-      // we need a signer here since this is a 'write; transaction.
+      // We need a Signer here since this is a 'write' transaction.
       const signer = await getProviderOrSigner(true);
-
-      // create a new instance of the contract with a signer which allows
+      // Create a new instance of the Contract with a Signer, which allows
       // update methods
       const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
-
-      // call the mint from the contract to ming the smokenft
-
+      // call the mint from the contract to mint the Smokenft
       const tx = await nftContract.mint({
-        // value signifies the cost of one smokenft which is '0.01' ethers
-        // we are parsing '0.01' string to ether using the utils library from ethers.js
-        value: utils.parseEther("0.01"),});
-
-        setLoading(true);
-        // wait for the transaction to get mined
-        await tx.wait();
-        setLoading(false);
-        window.alert("You succsesfully minted a Smokenft!");
-
-      }catch (err) {
-        console.error(err);
-      }
-  }
-
-  // connect wallet to metamask
-
-  const connectWallet = async () => {
-    try {
-      // get the provider from web3modal , which will come from metamask in our case
-      // when  used for the first time , it prompts the user to connect their wallet
-
-      await getProviderOnSigner();
-      setWalletConnected(true);
-    }catch (err) {
+        // value signifies the cost of one Smokenft which is "0.01" eth.
+        // We are parsing `0.01` string to ether using the utils library from ethers.js
+        value: utils.parseEther("0.01"),
+      });
+      setLoading(true);
+      // wait for the transaction to get mined
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully minted a Smokenft!");
+    } catch (err) {
       console.error(err);
     }
   };
 
-  // get tokenIdsMinted : get the number of tokenIds that have been minted
+  /*
+    connectWallet: Connects the MetaMask wallet
+  */
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
+  /**
+   * getTokenIdsMinted: gets the number of tokenIds that have been minted
+   */
   const getTokenIdsMinted = async () => {
-    try{
-
-      // get the provider from web3modal which will be metamask for this case
-      // no needed for the signer here, as we are only reading from the blockchain
-
-      const provider = await getProviderOnSigner();
-      // we connect to the Contract using a Provider , so we will only have read-only access to the contract
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
+      const provider = await getProviderOrSigner();
+      // We connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
       const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider);
       // call the tokenIds from the contract
       const _tokenIds = await nftContract.tokenIds();
       console.log("tokenIds", _tokenIds);
-      // _tokenIds is a 'Big Number ' we need to convert the number to a string 
+      //_tokenIds is a `Big Number`. We need to convert the Big Number to a string
       setTokenIdsMinted(_tokenIds.toString());
-
-    }catch (err) {
+    } catch (err) {
       console.error(err);
     }
-  }
+  };
 
-  /*
-  returns a provider or signer object representing the Ethereum RPC Without the signing capabilities
-
-  a provider need to interact with the blockchain - reading transactions , reading balances, reading state
-  a signer is a special type of provider used in case a writer transactions needs to be made to the blockchain which involves the connected acount
-
-
-  */
-
-  const getProviderOrSigner = async (needsigner = false) => {
-    // connect to metamask
-    // since we store 'web3modal'  as a reference we need to acces the 'current' value to get acces to the underlying object
+  /**
+   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+   * signing capabilities of metamask attached
+   *
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
+   * @param {*} needSigner - True if you need the signer, default false otherwise
+   */
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
     const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.web3Provider(provider);
-    // if user is not connected to the Mumbai network , let them know and throw an error
-    const { chainId } = await web3Provider.getNetwork();
-    if (chianId !== 80001) {
-      window.alert("Change the network to Mumbai");
-      throw new Error("Change the network to Mumbai");
+    const web3Provider = new providers.Web3Provider(provider);
 
+    // If user is not connected to the Mumbai network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 80001) {
+      window.alert("Change the network to Mumbai");
+      throw new Error("Change network to Mumbai");
+
+      // the above code checks for the networkid then it will use it to know if you are connected to mumbai 
+      // one can even change it to another network and only need to change the networkId
     }
+
     if (needSigner) {
       const signer = web3Provider.getSigner();
       return signer;
@@ -118,56 +114,79 @@ export default function Home() {
   };
 
   // useEffects are used to react to changes in state of the website
-
-  // the array at the end of the function call represents what statechanges will trigger this effect
-  // in this case, whenever the value of ' walletconnected' changes this effect will be called.
-
+  // The array at the end of function call represents what state changes will trigger this effect
+  // In this case, whenever the value of `walletConnected` changes - this effect will be called
   useEffect(() => {
-    // if wallet is not connected , create a new instance of web3Modal and connec the MetaMask wallet
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
     if (!walletConnected) {
-      // assign the web3Modal class to the reference object by setting it's 'current' value
-
-      // the 'current' value is persisted throughout as long as this page in open
-      web3ModalRef.current = new web3ModalRef({
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
         network: "mumbai",
         providerOptions: {},
         disableInjectedProvider: false,
-        
-      })
+      });
 
       connectWallet();
+
       getTokenIdsMinted();
 
       // set an interval to get the number of token Ids minted every 5 seconds
-
-      setInterval(async function (){
+      setInterval(async function () {
         await getTokenIdsMinted();
-
       }, 5 * 1000);
     }
-  }, [walletConnected]
-  )
-  
-  // render button : returns a button based on the state of the dapp
+  }, [walletConnected]);
 
+  /*
+    renderButton: Returns a button based on the state of the dapp
+  */
   const renderButton = () => {
-    // if wallet is connected , return a button which allows to connect their wallet
-
+    // If wallet is not connected, return a button which allows them to connect their wllet
     if (!walletConnected) {
       return (
-        <button onClick={connectWallet} className={styles.button}> Connect your wallet</button>
-      )
+        <button onClick={connectWallet} className={styles.button}>
+          Connect your wallet
+        </button>
+      );
     }
-  }
 
+    // If we are currently waiting for something, return a loading button
+    if (loading) {
+      return <button className={styles.button}>Loading...</button>;
+    }
 
-  
-
-
+    return (
+      <button className={styles.button} onClick={publicMint}>
+        Public Mint 🚀
+      </button>
+    );
+  };
 
   return (
-    <main className={styles.main}>
-     
-    </main>
-  )
+    <div>
+      <Head>
+        <title>Smokenft</title>
+        <meta name="description" content="Smokenft-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Smokenft!</h1>
+          <div className={styles.description}>
+            Its an NFT collection to be sold.
+          </div>
+          <div className={styles.description}>
+            {tokenIdsMinted}/11 have been minted
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="/Smokenft/Screenshot from 2023-06-03 02-05-21.png" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>Made with &#10084; by Smokenft</footer>
+    </div>
+  );
 }
